693eeb09dd548c904be077fe5d0cb3e67c0f43d3
Ata Tan Dagidir <tan.dagidir@ozu.edu.tr>
2025-05-04 14:42:03 +0300

    redid the IBidRepository

--- src/main/java/Data/repository/interfaces/IBidRepository.java
+++ src/main/java/Data/repository/interfaces/IBidRepository.java
@@ -1,12 +1,13 @@
 package Data.repository.interfaces;
 
 import Data.domain.Bid;
+
 import java.util.List;
 
 public interface IBidRepository {
-    void save(Bid bid);
-    boolean update(Bid bid);
-    Bid findById(String bidId);
-    List<Bid> findByPropertyId(String propertyId);
-
+    boolean addBid(Bid bid);
+    Bid getBidById(String bidId);
+    boolean updateBid(Bid bid);
+    List<Bid> getBidsByPropertyId(String propertyId);
+    List<Bid> getBidsByUserId(String userId);
 }
